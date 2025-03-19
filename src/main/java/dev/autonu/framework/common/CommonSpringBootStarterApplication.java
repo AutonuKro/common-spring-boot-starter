66c2d81cb8ace557caea3aaa53b65f945a77dca6
Autonu Kro <martinkro001@gmail.com>
2025-03-19 20:40:13 +0530

    Initial commit

---
+++ src/main/java/dev/autonu/framework/common/CommonSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package dev.autonu.framework.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonSpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonSpringBootStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/autonu/framework/common/CommonSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.autonu.framework.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonSpringBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
